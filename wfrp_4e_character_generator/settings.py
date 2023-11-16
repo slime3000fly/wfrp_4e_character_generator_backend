e480ca155abfd0f2ae9e4b94d64fca176ee45728
Piotr <piotr.kacprzak@likims.com>
2023-11-16 12:40:05 +0100

    work registry

--- wfrp_4e_character_generator/settings.py
+++ wfrp_4e_character_generator/settings.py
@@ -11,17 +11,22 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import dotenv
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Add .env variables
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c!os2j1+wc46@-g99u9a8re@y=8b05cnyw1t=!636yv5b-v@(h'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
