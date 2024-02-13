03a86f4d5d829fe7db22b17c59ea63227e0158d1
Piotr <piotr.kacprzak@likims.com>
2024-02-13 12:43:20 +0100

    endpoints!

--- wfrp_4e_character_generator/urls.py
+++ wfrp_4e_character_generator/urls.py
@@ -21,5 +21,7 @@ from .views import index, signup
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',index, name="index"),
-    path('signup/',signup, name="signup")
+    path('signup/',signup, name="signup"),
+    path('login/',login,name="login"),
+    path('save_json',save_json,name="save_json")
 ]
--- wfrp_4e_character_generator/views.py
+++ wfrp_4e_character_generator/views.py
@@ -18,3 +18,38 @@ def signup(request):
         return JsonResponse({"message": "registration succeeded"})
     else:
         return JsonResponse({"error": "method must be POST!"}, status=400)
+
+def login(request):
+    if request.method == "POST":
+        data = json.loads(request.body)
+        username = data.get("username")
+        password = data.get("password")
+        
+        user = User.objects.filter(username=username).first()  # get user 
+
+        if user and user.check_password(password):
+            return JsonResponse({"message": "login succeeded"})
+        else:
+            return JsonResponse({"error": "wrong username or password"}, status=400)
+
+    return JsonResponse({"error": "method must be POST!"}, status=400)
+
+def save_json(request):
+    if request.method == "POST":
+        data = json.loads(request.body)
+        username = data.get("username")
+        json_data = data.get("json_data")
+
+        user = User.objects.filter(username=username).first()  # get user
+
+        if user:
+            # update json objcet
+            user.profile.json_data = json_data
+            user.profile.save()
+
+            return JsonResponse({"message": "JSON data saved successfully"})
+        else:
+            return JsonResponse({"error": "User not found"}, status=400)
+
+    return JsonResponse({"error": "Method must be POST"}, status=400)
+
